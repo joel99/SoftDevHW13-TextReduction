8953312ea036eaf145bcf36e88ed5bbcf5c85f7a
Joel Ye <jye5@stuy.edu>
2017-05-02 15:32:52 -0400

    finish

--- bookProcessor.py
+++ bookProcessor.py
@@ -5,20 +5,27 @@ def groupFreq(wL, l):
     return reduce(lambda a,b: a + b, map(lambda x: 1 if x in wL else 0, l))
 
 #turn list into list of frequencies with list comp
-#def maxFreqWord(l):
-#    wordFreq = {}
-#    return reduce(lambda a,b: a if a > b else b, wordFreq)
+def maxFreqWord(l):
+    #wordFreq = {word: 1 if word not in wordFreq else word: wordFreq[word] + 1for word in l}
+    #ugh...
+    wordFreq = {}
+    for word in l:
+        if word in wordFreq:
+            wordFreq[word] += 1
+        else:
+            wordFreq[word] = 1
+    return reduce(lambda a,b: a if wordFreq[a] > wordFreq[b] else b, wordFreq.keys())
 
 
 def freqFile(w, filename):
     with open (filename, "r") as myfile:
         data=myfile.read()
-        print "Frequency of " + w + ": " + str(freq(w, data.split(" ")))
+        print "Frequency of '" + w + "': " + str(freq(w, data.split(" ")))
     
 def groupFreqFile(wL, filename):
     with open (filename, "r") as myfile:
         data=myfile.read()
-        print "Group frequency of " + wL + ": " + str(groupFreq(wL.split(" "), data.split(" ")))
+        print "Group frequency of '" + wL + "': " + str(groupFreq(wL.split(" "), data.split(" ")))
     
 def maxFreqWordFile(filename):
     with open (filename, "r") as myfile:
@@ -27,3 +34,5 @@ def maxFreqWordFile(filename):
 
 
 freqFile("the", "mobyDick.txt")
+groupFreqFile("Moby the", "mobyDick.txt")
+maxFreqWordFile("mobyDick.txt")
